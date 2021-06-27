888ef57aa848480c64ccacf0ce7717ab94902691
Róbert Bogár <bogar@astron.hu>
2021-06-27 11:13:07 +0200

    learn how manual DI works via - property injection - setter injection - constructor injection

---
+++ src/main/java/beetle/springframework/beetledi/controller/SetterInjectedController.java
@@ -0,0 +1,21 @@
+package beetle.springframework.beetledi.controller;
+
+import beetle.springframework.beetledi.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SetterInjectedController {
+
+    private GreetingService greetingService;
+
+    @Autowired
+    public void setGreetingService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting() {
+        return greetingService.sayGreeting();
+    }
+
+}
---
+++ src/test/java/beetle/springframework/beetledi/controller/ConstructorInjectedControllerTest.java
@@ -0,0 +1,22 @@
+package beetle.springframework.beetledi.controller;
+
+import beetle.springframework.beetledi.service.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ConstructorInjectedControllerTest {
+
+    ConstructorInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+//        controller = new ConstructorInjectedController(() -> "Hola!");
+        controller = new ConstructorInjectedController(new GreetingServiceImpl());
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(controller.getGreeting());
+    }
+
+}
