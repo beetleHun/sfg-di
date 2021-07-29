e542573f389e7e83ce93f904028d6c60896ecd58
Róbert Bogár <bogar@astron.hu>
2021-07-29 18:12:25 +0200

    spring boot application properties - remove datasource.properties reference from from config class to make SpringBoot gather the values from the application.properties

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -14,9 +14,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
 
-@PropertySource("classpath:datasource.properties")
 @Configuration
 @ImportResource("classpath:sfgdi-config.xml")
 public class GreetingServiceConfig {
